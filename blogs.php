44cb1cdf6f83e2b8ca3c7ff3401463cd31b1ce42
Karan Chittora <65993497+krnchittora2@users.noreply.github.com>
2021-03-17 14:08:46 +0530

    Add files via upload

---
+++ blogs.php
@@ -0,0 +1,155 @@
+
+<?php include 'header.php'; ?>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 text-center blogs_head">
+				<h1>Our Blogs</h1>
+				<hr>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="myBtnContainer">
+				  <button class="btn active" onclick="filterSelection('all')"> All</button>
+				  <button class="btn" onclick="filterSelection('cat-pratham')"> Pratham</button>
+				  <button class="btn" onclick="filterSelection('cat-hrc')"> HRC</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-lg-4 filterDiv cat-pratham">
+				<div class="blog_card">
+					<div class="blog_card_thumb">
+						<a href="https://theiinaniitian.wordpress.com/2017/09/26/the-making-of-pratham-satellite/" target="_blank"><img src="img/blogs/blog01.png"></a>
+					</div>
+					<div class="blog_card_date">
+						<span class="blog_card_date_day">26</span>
+						<span class="blog_card_date_month">Sep'17</span>
+					</div>
+					<div class="blog_card_body">
+						<div class="blog_card_category"><a href="https://theiinaniitian.wordpress.com/2017/09/26/the-making-of-pratham-satellite/" target="_blank">Pratham</a></div>
+						<div class="blog_card_title"><a href="https://theiinaniitian.wordpress.com/2017/09/26/the-making-of-pratham-satellite/" target="_blank">The Making of Pratham</a></div>
+						<div class="blog_card_subtitle"><i class="glyphicon glyphicon-user"></i>&nbsp;Yash Sanghvi</div>
+						<p class="blog_card_description">A couple of weeks after the launch of Pratham we were invited to a college for giving a talk. The organizers quite nervously asked us, “We have reserved a two hour slot for your talk...</p>
+					</div>
+					<div class="blog_card_footer">
+						<span class="read_more"><a href="https://theiinaniitian.wordpress.com/2017/09/26/the-making-of-pratham-satellite/" target="_blank">Read More</a></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 filterDiv cat-pratham">
+				<div class="blog_card">
+					<div class="blog_card_thumb">
+						<a href="http://anmoldiaries.blogspot.in/2017/09/when-satellite-launched-us.html" target="_blank"><img src="img/blogs/blog02.png"></a>	
+					</div>
+					<div class="blog_card_date">
+						<span class="blog_card_date_day">26</span>
+						<span class="blog_card_date_month">Sep'17</span>
+					</div>
+					<div class="blog_card_body">
+						<div class="blog_card_category"><a href="http://anmoldiaries.blogspot.in/2017/09/when-satellite-launched-us.html" target="_blank">Pratham</a></div>
+						<div class="blog_card_title"><a href="http://anmoldiaries.blogspot.in/2017/09/when-satellite-launched-us.html" target="_blank">When Satellite launched us!</a></div>
+						<div class="blog_card_subtitle"><i class="glyphicon glyphicon-user"></i>&nbsp;Anmol Sikka</div>
+						<p class="blog_card_description">There are some days which get etched in your memory. From a year earlier multiple memories were made and now a year hence here I am reliving and relishing the days...</p>
+					</div>
+					<div class="blog_card_footer">
+						<span class="read_more"><a href="http://anmoldiaries.blogspot.in/2017/09/when-satellite-launched-us.html" target="_blank">Read More</a></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 filterDiv cat-pratham">
+				<div class="blog_card">
+					<div class="blog_card_thumb">
+						<a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank"><img src="img/blogs/blog03.png"></a>
+					</div>
+					<div class="blog_card_date">
+						<span class="blog_card_date_day">28</span>
+						<span class="blog_card_date_month">Sep'17</span>
+					</div>
+					<div class="blog_card_body">
+						<div class="blog_card_category"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">Pratham</a></div>
+						<div class="blog_card_title"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">A 40 Hour Adventure</a></div>
+						<div class="blog_card_subtitle"><i class="glyphicon glyphicon-user"></i>&nbsp;Yash Sanghvi</div>
+						<p class="blog_card_description">More often than not, superlatives are found in the most unexpected of places. The purest form of beauty, for instance, can be found in simplicity. The purest form of happiness can be found in tears...</p>
+					</div>
+					<div class="blog_card_footer">
+						<span class="read_more"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">Read More</a></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 filterDiv cat-hrc">
+				<div class="blog_card">
+					<div class="blog_card_thumb">
+						<a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank"><img src="img/blogs/blog03.png"></a>
+					</div>
+					<div class="blog_card_date">
+						<span class="blog_card_date_day">28</span>
+						<span class="blog_card_date_month">Sep'17</span>
+					</div>
+					<div class="blog_card_body">
+						<div class="blog_card_category"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">HRC</a></div>
+						<div class="blog_card_title"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">HRC Blog 1</a></div>
+						<div class="blog_card_subtitle"><i class="glyphicon glyphicon-user"></i>&nbsp;Yash Sanghvi</div>
+						<p class="blog_card_description">More often than not, superlatives are found in the most unexpected of places. The purest form of beauty, for instance, can be found in simplicity. The purest form of happiness can be found in tears...</p>
+					</div>
+					<div class="blog_card_footer">
+						<span class="read_more"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">Read More</a></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 filterDiv cat-hrc">
+				<div class="blog_card">
+					<div class="blog_card_thumb">
+						<a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank"><img src="img/blogs/blog03.png"></a>
+					</div>
+					<div class="blog_card_date">
+						<span class="blog_card_date_day">28</span>
+						<span class="blog_card_date_month">Sep'17</span>
+					</div>
+					<div class="blog_card_body">
+						<div class="blog_card_category"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">HRC</a></div>
+						<div class="blog_card_title"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">HRC Blog 2</a></div>
+						<div class="blog_card_subtitle"><i class="glyphicon glyphicon-user"></i>&nbsp;Yash Sanghvi</div>
+						<p class="blog_card_description">More often than not, superlatives are found in the most unexpected of places. The purest form of beauty, for instance, can be found in simplicity. The purest form of happiness can be found in tears...</p>
+					</div>
+					<div class="blog_card_footer">
+						<span class="read_more"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">Read More</a></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-lg-4 filterDiv cat-hrc">
+				<div class="blog_card">
+					<div class="blog_card_thumb">
+						<a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank"><img src="img/blogs/blog03.png"></a>
+					</div>
+					<div class="blog_card_date">
+						<span class="blog_card_date_day">28</span>
+						<span class="blog_card_date_month">Sep'17</span>
+					</div>
+					<div class="blog_card_body">
+						<div class="blog_card_category"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">HRC</a></div>
+						<div class="blog_card_title"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">HRC Blog 3</a></div>
+						<div class="blog_card_subtitle"><i class="glyphicon glyphicon-user"></i>&nbsp;Yash Sanghvi</div>
+						<p class="blog_card_description">More often than not, superlatives are found in the most unexpected of places. The purest form of beauty, for instance, can be found in simplicity. The purest form of happiness can be found in tears...</p>
+					</div>
+					<div class="blog_card_footer">
+						<span class="read_more"><a href="https://theiinaniitian.wordpress.com/2017/09/28/a-40-hour-adventure-prathams-thermo-vacuum-test/" target="_blank">Read More</a></span>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+<?php include 'footer.php'; ?>
